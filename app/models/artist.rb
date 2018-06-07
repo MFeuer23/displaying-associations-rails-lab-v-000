cbefcd8e8fd056afe8f1802b40b71e6c87dc1707
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-07 23:01:18 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+<<<<<<< HEAD
 
+=======
+  
+  
+
+  
+>>>>>>> 13efaf0ec1142f39ccafbff86a2e35879e48c0e3
   def song_count
     songs.count
   end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+<<<<<<< HEAD
 
+=======
+  
+
+  
+>>>>>>> 13efaf0ec1142f39ccafbff86a2e35879e48c0e3
   def artist_name
     artist.name
   end
